04a5479297cd0311e3f19f61a291b29119098e82
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-03-10 10:09:52 -0300

    controller

---
+++ personmanagementApi/src/main/java/com/inacioalves/personmanagementApi/PersonmanagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.inacioalves.personmanagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonmanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonmanagementApiApplication.class, args);
+	}
+
+}
